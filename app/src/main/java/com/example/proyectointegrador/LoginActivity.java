5fc871ca8f229c0ea198a8290a45492edf13325f
Saloescova <saloescobarvallejo@gmail.com>
2024-09-11 12:48:44 -0500

    Proyecto Integrador

---
+++ app/src/main/java/com/example/proyectointegrador/LoginActivity.java
@@ -0,0 +1,32 @@
+package com.example.proyectointegrador;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button loginButton = findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Implement login logic here
+                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+              }
+            });
+    }
+}
